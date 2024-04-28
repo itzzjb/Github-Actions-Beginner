f26f398c63eba8df78afb429b3ff3616f82f9a88
Januda Bethmin <desilvabethmin@gmail.com>
2024-04-28 11:04:25 +0530

    formatting code

--- app/main_test.py
+++ app/main_test.py
@@ -2,6 +2,7 @@ from main import return_backwards_string, get_mode
 import unittest
 import os
 
+
 class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
         random_string = "This is my test string"
@@ -10,6 +11,7 @@ class TestMain(unittest.TestCase):
 
     def test_get_mode(self):
         self.assertEqual(os.environ.get('MODE'), get_mode())
+
 
 if __name__ == '__main__':
     unittest.main()
